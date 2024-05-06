6019008e9be941d781e0a9d45f596e87fa892482
Seungmin-L <s3864235@rmit.edu.vn>
2024-05-07 01:32:17 +0700

    Finalize the encoding, normalization, rescaling

--- utils/encoding.py
+++ utils/encoding.py
@@ -1,5 +1,21 @@
 import matplotlib.pyplot as plt
+import pandas as pd
+
 from sklearn.preprocessing import LabelEncoder
+
+
+def one_hot_encode(df, column_name):
+    """
+    Perform one-hot encoding on a specified column in a DataFrame.
+    
+    Parameters:
+    df (pd.DataFrame): The DataFrame on which to perform encoding.
+    column_name (str): The name of the column to encode.
+    
+    Returns:
+    pd.DataFrame: A DataFrame with the original column replaced by one-hot encoded columns.
+    """
+    return pd.get_dummies(df, columns=[column_name])
 
 def label_encode(df, column_name):
     """
--- utils/rescale_image.py
+++ utils/rescale_image.py
@@ -1,35 +1,46 @@
-import pandas as pd
 import os
+import io
+import random
+import logging
 
-from PIL import Image
 from io import BytesIO
+from PIL import Image
 
 
-def resize_image(image_path, base_path, size=(350, 350)):
+def resize_image(image_path, base_path, size=(200, 200)):
     full_path = os.path.join(base_path, image_path)
     try:
         with Image.open(full_path) as img:
             if img.size != size:
                 img = img.resize(size, Image.Resampling.LANCZOS)
-                img_byte_arr = BytesIO()
-                img.save(img_byte_arr, format='JPEG')
-                resized_data = img_byte_arr.getvalue()
-            else:
-                img_byte_arr = BytesIO()
-                img.save(img_byte_arr, format='JPEG')
-                resized_data = img_byte_arr.getvalue()
-            return resized_data, img.size
+            img_byte_arr = BytesIO()
+            img.save(img_byte_arr, format='JPEG')
+            resized_data = img_byte_arr.getvalue()
+            return resized_data
     except Exception as e:
-        print(f"Error resizing image {full_path}: {e}")
-        return None, (0, 0)
+        logging.error(f"Error resizing image {full_path}: {e}")
+        return None
 
 
-def resize_images_in_dataframe(df, base_path, size=(350, 350)):
-    results = df['Path'].apply(lambda x: resize_image(x, base_path, size))
-
-    # Update df's numerical columns based on the resized results
-    df['ResizedImage'], sizes = zip(*results)
-    df['Width'], df['Height'] = zip(*sizes)
-    df['Ratio'] = df['Width'] / df['Height']
-
+def resize_images_in_dataframe(df, base_path, size=(200, 200)):
+    df['ResizedImage'] = df['Path'].apply(lambda x: resize_image(x, base_path, size))
     return df
+
+
+def load_random_images(df, num_images=3):
+    random_indices = random.sample(range(len(df)), num_images)
+    random_images = df.iloc[random_indices]
+    return random_images
+
+
+def display_random_images(df, num_images=3):
+    random_images = load_random_images(df, num_images)
+    
+    for _, row in random_images.iterrows():
+        image_data = row['ResizedImage']
+        image_path = row['Path']
+        
+        print(f"Image Path: {image_path}")
+        
+        image = Image.open(io.BytesIO(image_data))
+        display(image)
