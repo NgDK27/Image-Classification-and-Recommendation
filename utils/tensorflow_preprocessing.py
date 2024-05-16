469c173b81931775041374ae2278c83f2cd88403
NgDK27 <85181668+NgDK27@users.noreply.github.com>
2024-05-17 02:38:00 +0700

    Update for placeholding

--- utils/tensorflow_preprocessing.py
+++ utils/tensorflow_preprocessing.py
@@ -8,21 +8,8 @@ from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from utils.augmentation import augment_image
 
 
-def show_batch(image_batch, path_batch):
-    """Visualize an image batch along with their paths."""
-    plt.figure(figsize=(15, 15))
-    for i in range(min(9, image_batch.shape[0])):  # Ensure not to exceed batch size or 9
-        ax = plt.subplot(3, 3, i + 1)
-        plt.imshow(image_batch[i])
-        path_info = textwrap.fill(path_batch[i], width=50)  # Wrap text, no need to decode
-        plt.xlabel(path_info, fontsize=8)  # Display the path below the image
-        plt.xticks([])  # Remove x-axis ticks
-        plt.yticks([])  # Remove y-axis ticks
-    plt.tight_layout()
-    plt.show()
 
-
-def process_image_from_path(image_path, img_height, img_width, to_augment="Unique"):
+def process_image_from_path(image_path, img_height, img_width, to_augment="Unique", for_model=False):
     # Read image
     img = tf.io.read_file(image_path)
 
@@ -40,6 +27,9 @@ def process_image_from_path(image_path, img_height, img_width, to_augment="Uniqu
     # Rescale pixel value
     rescaling_layer = tf.keras.layers.Rescaling(scale=1. / 255)
     img = rescaling_layer(img)
+
+    if for_model:
+        img = tf.expand_dims(img, axis=0)
 
     return img
 
@@ -124,26 +114,3 @@ def prepare_image_dataset(df, img_height=256, img_width=256, batch_size=32, base
     image_ds = image_ds.batch(batch_size)
 
     return image_ds
-
-def process_image_for_model(image_path, img_height, img_width, to_augment="Unique"):
-    # Read image
-    img = tf.io.read_file(image_path)
-
-    # Decode to RGB
-    img = tf.io.decode_jpeg(img, channels=3)
-
-    # Resize
-    img = tf.image.resize(img, [img_height, img_width])
-
-    # Augment only images marked as "Duplicate"
-    is_duplicate = tf.equal(to_augment, "Duplicate")
-
-    img = tf.cond(is_duplicate, lambda: augment_image(img), lambda: img)
-
-    # Rescale pixel value
-    rescaling_layer = tf.keras.layers.Rescaling(scale=1. / 255)
-    img = rescaling_layer(img)
-
-    img = tf.expand_dims(img, axis=0)
-
-    return img
